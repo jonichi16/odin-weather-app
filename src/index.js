001d2711da5b64e9a3086926fb25e33712de4b32
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-23 21:04:28 +0800

    feat: Add weather display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './stylesheets/reset.css';
 import './stylesheets/styles.css';
+import getWeather from './javascript/weather';
 import render from './javascript/components/dom';
 import header from './javascript/components/header';
 import main from './javascript/components/main';
 import form from './javascript/components/search-form';
+import weatherCard from './javascript/components/weather-card';
 import footer from './javascript/components/footer';
 
 const content = document.querySelector('.content');
@@ -13,3 +15,29 @@ const children = [
   footer.createFooter(),
 ];
 render(content, children);
+
+const searchInput = document.querySelector('.search-input');
+const searcButton = document.querySelector('.search-btn');
+
+const displayWeather = async () => {
+  const container = document.createElement('div');
+
+  try {
+    const weather = await getWeather(searchInput.value.toLowerCase());
+    container.classList.add('weather');
+    weatherCard.createWeatherCard(container, weather);
+  } catch (e) {
+    container.classList.add('error');
+    container.textContent = 'Not Found';
+  }
+
+  main.displayMain(container);
+};
+
+searcButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  displayWeather();
+
+  searchInput.value = '';
+});
---
+++ src/javascript/components/weather-card.js
@@ -0,0 +1,23 @@
+const city = document.createElement('h2');
+city.classList.add('city');
+const condition = document.createElement('p');
+condition.classList.add('condition');
+const icon = document.createElement('img');
+icon.classList.add('icon');
+const temperature = document.createElement('p');
+temperature.classList.add('temp');
+
+const createWeatherCard = (container, weather) => {
+  city.textContent = weather.location.name;
+  condition.textContent = weather.current.condition.text;
+  icon.src = weather.current.condition.icon;
+  temperature.textContent = weather.current.temp_c;
+
+  container.append(city, condition, icon, temperature);
+};
+
+const weatherCard = {
+  createWeatherCard,
+};
+
+export default weatherCard;
