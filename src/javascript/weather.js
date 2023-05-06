f8763bb8fd32a9f300933a48ea6aace5d55a0394
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-06 15:01:52 +0800

    feat: Add mode cors on fetch

--- src/javascript/weather.js
+++ src/javascript/weather.js
@@ -1,6 +1,9 @@
 const getWeather = async (location) => {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=c8c89f0cbb4e432db7c181806232204&q=${location}/`
+    `https://api.weatherapi.com/v1/current.json?key=c8c89f0cbb4e432db7c181806232204&q=${location}/`,
+    {
+      mode: 'cors',
+    }
   );
   const data = await response.json();
   return data;
